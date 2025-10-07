31df29c92093c8c4fd1fd5674e4c2781bbb12fa6
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-10-07 13:10:48 +0300

    formatted import statements

--- populatedb.py
+++ populatedb.py
@@ -1,6 +1,6 @@
-from typing import List
 from pymongo import MongoClient
 from pymongo.database import Collection
+
 from app.model.employee import Employee
 from app.model.product import Product
 from app.model.supervisor import Supervisor
