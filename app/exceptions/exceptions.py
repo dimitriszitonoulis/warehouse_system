42adf05ad1fee4bc9bacae69bec7a6b091500d80
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-11-24 21:24:29 +0200

    minor formating changes

--- app/exceptions/exceptions.py
+++ app/exceptions/exceptions.py
@@ -3,15 +3,19 @@ from typing import Optional
 
 # User exceptions
 class UserNotFoundByIdError(Exception):
-    """ Raised when a user cannot be found by their id. """
+    """Raised when a user cannot be found by their id."""
+
     def __init__(self, user_id):
         super().__init__(f"Employee with id={user_id} does not exist.")
 
 
 class UserNotFoundByCredentialsError(Exception):
-    """ Raised when a user cannot be found by their credentials. """
+    """Raised when a user cannot be found by their credentials."""
+
     def __init__(self, username: str, unit_id: Optional[str] = None):
-        error: str = f"User with username={username} in unit with id={unit_id} was not found."
+        error: str = (
+            f"User with username={username} in unit with id={unit_id} was not found."
+        )
         if unit_id is not None:
             error = f"User with username={username} was not found."
         super().__init__(error)
@@ -19,15 +23,19 @@ class UserNotFoundByCredentialsError(Exception):
 
 # Product exceptions
 class ProductNotFoundByIdError(Exception):
-    """ Raised when a product cannot be found by its id. """
+    """Raised when a product cannot be found by its id."""
+
     def __init__(self, product_id):
         super().__init__(f"Product with id={product_id} does not exist.")
 
 
 class ProductDoesNotFitInUnit(Exception):
-    """ Raised when the given product does not fit in the given unit. """
+    """Raised when the given product does not fit in the given unit."""
+
     def __init__(self, product_id: str, unit_id: str):
-        super().__init__(f"Product with id={product_id} does not fit into unit with id={unit_id}")
+        super().__init__(
+            f"Product with id={product_id} does not fit into unit with id={unit_id}"
+        )
 
 
 class InsufficientProductQuantity(Exception):
@@ -36,11 +44,16 @@ class InsufficientProductQuantity(Exception):
         - trying to sell more items than what is available the database.
         - `quantity_to_sell` is negative
     """
+
     def __init__(self, product_id: str, quantity_to_sell: str):
-        super().__init__(f"Cannot sell {quantity_to_sell} items of product with id={product_id}.")
+        super().__init__(
+            f"Cannot sell {quantity_to_sell} items of product with id={product_id}."
+        )
+
 
 # Unit exceptions
 class UnitNotFoundByIdError(Exception):
-    """ Raised when a unit cannot be found by its id. """
+    """Raised when a unit cannot be found by its id."""
+
     def __init__(self, unit_id):
         super().__init__(f"Unit with id={unit_id} does not exist.")
